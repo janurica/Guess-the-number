0b6713a678719dfc24f3bc5f22274f168f1a731c
janurica <61921258+janurica@users.noreply.github.com>
2021-06-28 16:43:06 +0200

    Final steps

--- script.js
+++ script.js
@@ -3,6 +3,37 @@ let userGuesses = [];
 let attempts = 0;
 let maxGuesses;
 
+let low = 1;
+let high = 100;
+
+function updateRange(){
+  const rangeOutput = document.getElementById("rangeOutput");
+        rangeOutput.innerText = `${low} - ${high}`
+        rangeOutput.style.marginLeft = low + " %";
+        rangeOutput.style.marginRight = 100 - high + " % ";
+        rangeOutput.classList.add("flash");
+        
+  const lowValue = document.getElementById("low");
+          lowValue.style.flex = low + " % ";
+          lowValue.style.background = "#ef7b54";
+  const space = document.getElementById("space");
+         space.style.flex = high-low + " % ";
+         space.style.background = "#83e1d0";
+  const highValue = document.getElementById("high");
+         highValue.style.flex = 100 - high + " % ";
+         highValue.style.background = "#ef7b54";
+}
+
+function gameEnded(){
+ document.getElementById('newGameButton').style.display="inline";
+ document.getElementById("inputBox").setAttribute("readonly","readonly");
+
+}
+
+function newGame() {
+  window.location.reload();
+}
+
 function init(){
   computerGuess = Math.floor(Math.random()*100+1);
   console.log(computerGuess);
@@ -34,23 +65,29 @@ function compareGuess(){
 document.getElementById("attempts").innerHTML = attempts;
 if(attempts< maxGuesses){
   if(userGuess > computerGuess){
+    if(userGuess< high) high = userGuess;
+
     document.getElementById('textOutput').innerHTML = 'Your guess is too high';
     document.getElementById('inputBox').value = "";
   }else if(userGuess < computerGuess){
+    if(userGuess>low) low = userGuess;
     document.getElementById('textOutput').innerHTML = 'Your guess is too low';
     document.getElementById('inputBox').value = "";
   }else {
     document.getElementById('textOutput').innerHTML = "Correct You've got it in:"+attempts+ " attempts";
+    gameEnded();
   }
 }else {
   if(userGuess > computerGuess){
     document.getElementById('textOutput').innerHTML = "YOU LOOSE!.<br> The number was: "+computerGuess;
-
+    gameEnded();
   }else if(userGuess < computerGuess){
     document.getElementById('textOutput').innerHTML = "YOU LOOSE!.<br> The number was: "+computerGuess;
-
+    gameEnded();
   }else {
-    document.getElementById('textOutput').innerHTML = "Correct You've got it in:"+attempts+ " attempts";
+    document.getElementById('textOutput').innerHTML = "Correct You've got it in: "+attempts+ " attempts";
+    gameEnded();
   }
 }
+  updateRange();
 }
